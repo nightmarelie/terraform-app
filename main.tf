85d12585e6c4fc806770ba483f40e24c53d572ae
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-08-10 12:58:30 -0600

    feat: add existing vpc

--- main.tf
+++ main.tf
@@ -13,3 +13,15 @@ resource "aws_subnet" "dev-subnet-1" {
   cidr_block        = "10.0.10.0/24"
   availability_zone = "ca-central-1a"
 }
+
+data "aws_vpc" "existing_vpc" {
+  default = true
+}
+
+
+
+resource "aws_subnet" "dev-subnet-2" {
+  vpc_id            = data.aws_vpc.existing_vpc.id
+  cidr_block        = "172.31.48.0/20"
+  availability_zone = "ca-central-1a"
+}
