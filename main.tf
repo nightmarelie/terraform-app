5c5338610f91722ee63d3f18ddde50c012504ed4
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-08-10 12:44:58 -0600

    feat: add aws providers

--- main.tf
+++ main.tf
@@ -3,3 +3,13 @@ provider "aws" {
   access_key = ""
   secret_key = ""
 }
+
+resource "aws_vpc" "dev-vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "dev-subnet-1" {
+  vpc_id            = aws_vpc.dev-vpc.id
+  cidr_block        = "10.0.10.0/24"
+  availability_zone = "ca-central-1a"
+}
