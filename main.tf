c77cc131ce2ed9084d632e60e6c8654915470b63
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-08-10 21:53:24 -0600

    feat: create c2 instance

--- main.tf
+++ main.tf
@@ -19,6 +19,8 @@ variable "available_zone" {
 }
 
 variable "my_ip" {}
+variable "instance_type" {}
+variable "public_key_location" {}
 
 resource "aws_vpc" "dev-app-vpc" {
   cidr_block = var.vpc_cidr_block
@@ -108,4 +110,44 @@ resource "aws_default_security_group" "default-sg" {
   }
 }
 
+data "aws_ami" "latest-amazon-linux-image" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
 
+output "aws_ami" {
+  value = data.aws_ami.latest-amazon-linux-image.id
+}
+
+resource "aws_key_pair" "ssh-key" {
+  key_name   = "server-key-pair"
+  public_key = file(var.public_key_location)
+}
+
+
+
+resource "aws_instance" "dev-app-server" {
+  ami           = data.aws_ami.latest-amazon-linux-image.id
+  instance_type = var.instance_type
+
+  subnet_id              = aws_subnet.dev-app-subnet-1.id
+  vpc_security_group_ids = [aws_default_security_group.default-sg.id]
+  availability_zone      = var.available_zone
+
+  associate_public_ip_address = true
+  key_name                    = "server-key-pair"
+
+  user_data = file("entry-script.sh")
+
+  tags = {
+    Name = "${var.environment}-server"
+  }
+}
