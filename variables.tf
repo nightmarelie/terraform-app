1e3ff1a2b4d4c8e931714618e5fb215b6c6642dd
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-08-11 13:32:04 -0600

    feat: separate variables

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "vpc_cidr_block" {
+  description = "vpc cidr block"
+}
+
+variable "subnet_cidr_block" {
+  description = "subnet cidr block"
+}
+
+variable "environment" {
+  description = "environment"
+}
+
+variable "available_zone" {
+  description = "available zone"
+}
+
+variable "my_ip" {}
+variable "instance_type" {}
+variable "public_key_location" {}
+variable "private_key_location" {}
