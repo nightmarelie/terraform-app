02de96cfb7723af69ad218458227913b0d7c7440
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-08-10 22:10:09 -0600

    feat: create key pair

--- main.tf
+++ main.tf
@@ -128,10 +128,13 @@ output "aws_ami" {
 }
 
 resource "aws_key_pair" "ssh-key" {
-  key_name   = "server-key-pair"
+  key_name   = "server-key"
   public_key = file(var.public_key_location)
 }
 
+output "ec2_public_ip" {
+  value = aws_instance.dev-app-server.public_ip
+}
 
 
 resource "aws_instance" "dev-app-server" {
@@ -143,9 +146,9 @@ resource "aws_instance" "dev-app-server" {
   availability_zone      = var.available_zone
 
   associate_public_ip_address = true
-  key_name                    = "server-key-pair"
+  key_name                    = aws_key_pair.ssh-key.key_name
 
-  user_data = file("entry-script.sh")
+  # user_data = file("entry-script.sh")
 
   tags = {
     Name = "${var.environment}-server"
